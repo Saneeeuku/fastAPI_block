a593f10160f60e58f518940407b405274c15ce88
Alex <sharkline942@gmail.com>
2025-08-31 15:34:54 +0300

    first step commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def func():
+    return "Hello world"
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", reload=True)
