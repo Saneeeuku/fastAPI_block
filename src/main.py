a44786813e4e04ca9217486495fa7f549bd96d0e
AlexKsky <sharkline942@gmail.com>
2025-09-03 14:34:14 +0300

    added model folder, change env file path

--- src/main.py
+++ src/main.py
@@ -1,9 +1,5 @@
 import uvicorn
 from fastapi import FastAPI
-import sys
-from pathlib import Path
-
-sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as router_hotels
 from src.config import settings
