40f7915ee70a1910a0a8b08a5ec8c2d6f6186ee9
AlexKsky <sharkline942@gmail.com>
2025-10-17 19:16:17 +0300

    delete fxtures options in pytest.ini, make asyncclient and db standalone fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,58 +1,65 @@
 import json
-from pathlib import Path
 
 import pytest
 from httpx import AsyncClient, ASGITransport
-from sqlalchemy import insert
 
 from src.main import app
 from src.config import settings
-from src.database import Base, engine_null_pool
+from src.database import Base, engine_null_pool, async_new_session_null_pool
 from src.models import *
+from src.schemas.hotels_schemas import HotelAdd
+from src.schemas.rooms_schemas import RoomAdd
+from src.utils.db_manager import DBManager
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def check_test_mode():
     assert settings.MODE == "TEST"
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture()
+async def db() -> DBManager:
+    async with DBManager(session_factory=async_new_session_null_pool) as db:
+        yield db
+
+
+@pytest.fixture(scope="session", autouse=True)
 async def setup_db(check_test_mode) -> None:
-    hotels_data = receive_data_from_json("mock_hotels")
-    rooms_data = receive_data_from_json("mock_rooms")
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
-        await conn.execute(insert(HotelsOrm).values(hotels_data))
-        await conn.execute(insert(RoomsOrm).values(rooms_data))
+
+    hotels_data = receive_data_from_json("mock_hotels.json")
+    rooms_data = receive_data_from_json("mock_rooms.json")
+
+    async with DBManager(session_factory=async_new_session_null_pool) as db:
+        await db.hotels.add_bulk([HotelAdd.model_validate(h) for h in hotels_data])
+        await db.rooms.add_bulk([RoomAdd.model_validate(r) for r in rooms_data])
+        await db.commit()
 
 
-@pytest.fixture(autouse=True)
-async def create_user(setup_db) -> None:
+@pytest.fixture(scope="session")
+async def ac() -> AsyncClient:
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
-        await ac.post(
-            "/auth/registration",
-            json={
-                "email": "qwerty@mail.com",
-                "password": "strongpassword",
-                "nickname": "coolnickname"
-            }
-        )
+        yield ac
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def create_user(setup_db, ac) -> None:
+    await ac.post(
+        "/auth/registration",
+        json={
+            "email": "qwerty@mail.com",
+            "password": "strongpassword",
+            "nickname": "coolnickname"
+        }
+    )
 
 
 def receive_data_from_json(json_filename: str) -> list[dict]:
-    """Json files should be placed in project root folder
-    :arg str json_filename: name without extension
+    """Json files should be placed in tests folder
+    :arg str json_filename: name with extension
     """
-    full_path_to_json = _paths_to_json_file(json_filename)
-    with open(full_path_to_json, mode="r", encoding='utf-8') as f:
+    with open(f"./tests/{json_filename}", mode="r", encoding='utf-8') as f:
         file = json.load(f)
     return file
-
-
-def _paths_to_json_file(json_filename: str):
-    project_root_name = "fastAPI_block"
-    root_path = Path(__file__)
-    while root_path.name != project_root_name:
-        root_path = root_path.parent
-    return root_path.joinpath(f"./{json_filename}.json")
--- tests/intergation_tests/hotels/test_db.py
+++ tests/intergation_tests/hotels/test_db.py
@@ -1,10 +1,7 @@
-from src.database import async_new_session_null_pool
 from src.schemas.hotels_schemas import HotelAdd
-from src.utils.db_manager import DBManager
 
 
-async def test_hotel_add():
+async def test_hotel_add(db):
     data = HotelAdd(title="title1", location="location1")
-    async with DBManager(session_factory=async_new_session_null_pool) as db:
-        await db.hotels.add(data)
-        await db.commit()
+    await db.hotels.add(data)
+    await db.commit()
